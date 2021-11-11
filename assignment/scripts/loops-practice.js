0a17bfe9bffa342a5dd834e3d2718384ac534979
Chris Agbolade <yemluck@gmail.com>
2021-11-11 02:51:42 -0600

    Question 1 verified

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -34,7 +34,7 @@ for (let i=2; i<11; i+=2) {
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 for (let i=5; i>=0; i--){
-  console.log(i); // I tried i is greater than -1
+  console.log(i); // I tried i is greater than -1 and it worked too
 }
 
 
